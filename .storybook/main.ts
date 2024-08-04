ede404aea812ccce2314e4bea6f75b65334b552b
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-04 17:02:48 +0900

    chore : storybookのセットアップ

---
+++ .storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+const path = require('path')
+
+const config: StorybookConfig = {
+  stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    'storybook-addon-pseudo-states',
+  ],
+  /**
+   * @see https://github.com/storybookjs/storybook/issues/3916#issuecomment-871283551
+   * @see https://github.com/storybookjs/storybook/discussions/25470
+   */
+  webpackFinal(config: any) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../app'),
+    }
+    return config
+  },
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+}
+export default config
---
+++ .storybook/preview.tsx
@@ -0,0 +1,36 @@
+import type { Preview } from '@storybook/react'
+
+/** @see https://stackoverflow.com/questions/76933793/how-to-write-decorator-in-typescript-for-storybook */
+import React from 'react'
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <div style={{ width: '576px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    pseudo: {
+      hover: '#hover',
+      active: '#active',
+      focusVisible: '#focus-visible',
+    },
+    nextjs: {
+      /**
+       * next/navigationを有効にする場合下記のセットアップが必要になる
+       * @see https://storybook.js.org/docs/get-started/nextjs#set-nextjsappdirectory-to-true
+       */
+      appDirectory: true,
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview

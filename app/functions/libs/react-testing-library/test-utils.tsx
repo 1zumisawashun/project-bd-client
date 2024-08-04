093a46f10ce40468bca8b1cc8cb031ed8a8816c0
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-04 16:50:27 +0900

    chore : testのセットアップ

---
+++ app/functions/libs/react-testing-library/test-utils.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { render, RenderOptions } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider'
+
+/** @description renderに必要なProviderはここでまとめて提供する */
+function AllTheProviders({ children }: { children: React.ReactNode }) {
+  /**
+   * @description next/linkのモックを提供する
+   * @see https://www.npmjs.com/package/next-router-mock
+   */
+  return <MemoryRouterProvider>{children}</MemoryRouterProvider>
+}
+
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => {
+  return {
+    user: userEvent.setup(),
+    ...render(ui, { wrapper: AllTheProviders, ...options }),
+  }
+}
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }

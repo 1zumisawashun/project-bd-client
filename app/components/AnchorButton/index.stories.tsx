ccf8fd29eccff1ab3b5c018c905c95455bd726c5
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-09 19:36:44 +0900

    ui : anchor-buttonの作成

---
+++ app/components/AnchorButton/index.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { AnchorButton } from './index'
+
+const meta: Meta<typeof AnchorButton> = {
+  title: 'AnchorButton',
+  component: AnchorButton,
+}
+
+export default meta
+
+type Story = StoryObj<typeof AnchorButton>
+
+function Render() {
+  const items = [
+    { id: 1, variant: 'contained', theme: 'primary' },
+    { id: 2, variant: 'outlined', theme: 'primary' },
+    { id: 3, variant: 'contained', theme: 'danger' },
+    { id: 4, variant: 'outlined', theme: 'danger' },
+  ] as const
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      {items.map((item) => (
+        <div
+          style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}
+          key={item.id}
+        >
+          <AnchorButton href="#" variant={item.variant} theme={item.theme}>
+            default
+          </AnchorButton>
+          <AnchorButton
+            href="#"
+            variant={item.variant}
+            theme={item.theme}
+            id="hover"
+          >
+            hover
+          </AnchorButton>
+          <AnchorButton
+            href="#"
+            variant={item.variant}
+            theme={item.theme}
+            id="active"
+          >
+            active
+          </AnchorButton>
+          <AnchorButton
+            href="#"
+            variant={item.variant}
+            theme={item.theme}
+            id="focus-visible"
+          >
+            focus visible
+          </AnchorButton>
+          <AnchorButton
+            href="#"
+            variant={item.variant}
+            theme={item.theme}
+            disabled
+          >
+            disabled
+          </AnchorButton>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const Default: Story = {
+  args: {},
+  render: Render,
+}

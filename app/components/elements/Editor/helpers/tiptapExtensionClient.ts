ccc4c7dfdd48b98009ba026c8594e4b7b692f204
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-18 14:59:01 +0900

    add github actions timeline

--- app/components/elements/Editor/helpers/tiptapExtensionClient.ts
+++ app/components/elements/Editor/helpers/tiptapExtensionClient.ts
@@ -208,9 +208,9 @@ class SelectionSizeTooltip {
 }
 
 /**
- * state.selectionに関してはこれに詳細書いてある > @see https://benborgers.com/posts/tiptap-selection
+ * state.selectionに関してはこれに詳細書いてある >  @see https://benborgers.com/posts/tiptap-selection
  * state.selection.emptyはスクロール選択のこと
- * 参考にしたのはこちら -> @see https://stackoverflow.com/questions/73739904/how-can-i-upload-files-in-tip-tap-editor-for-react
+ * 参考にしたのはこちら ->  @see https://stackoverflow.com/questions/73739904/how-can-i-upload-files-in-tip-tap-editor-for-react
  */
 export const EventHandler = Extension.create({
   name: 'eventHandler',

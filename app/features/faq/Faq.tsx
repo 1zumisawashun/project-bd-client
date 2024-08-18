9004e361adaee496926f27378755bef7b6226e7b
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-18 19:52:18 +0900

    chore : fix eslint config import

--- app/features/faq/Faq.tsx
+++ app/features/faq/Faq.tsx
@@ -2,10 +2,4 @@ import { Nl2br } from '@/components/elements/Nl2br'
 import { faq } from './Faq.constant'
 import 'server-only'
 
-enum FaqCategory {
-  General = 'General',
-  Account = 'Account',
-  Payment = 'Payment',
-}
-
 export const Faq: React.FC = () => <Nl2br>{faq}</Nl2br>

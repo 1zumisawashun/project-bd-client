bd7ca22f3bc48ec5df768f271b60406a19e82e00
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-18 16:35:12 +0900

    add enum

--- app/features/faq/Faq.tsx
+++ app/features/faq/Faq.tsx
@@ -2,4 +2,10 @@ import { Nl2br } from '@/components/elements/Nl2br'
 import { faq } from './Faq.constant'
 import 'server-only'
 
+enum FaqCategory {
+  General = 'General',
+  Account = 'Account',
+  Payment = 'Payment',
+}
+
 export const Faq: React.FC = () => <Nl2br>{faq}</Nl2br>

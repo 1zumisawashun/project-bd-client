3d312c36c55273d1d732d688d17d9ab284e29745
1zumisawashun <shun.1zumisawa@gmail.com>
2024-08-30 23:33:10 +0900

    remove : subHeaderを削除する

--- app/(pages)/articles/create/layout.tsx
+++ app/(pages)/articles/create/layout.tsx
@@ -1,5 +1,4 @@
 import { SiteWrapper } from '@/components/layouts/SiteWrapper'
-import { SubHeader } from '@/components/layouts/SubHeader'
 import { Metadata } from 'next'
 
 const title = 'Article Create'
@@ -7,11 +6,5 @@ export const metadata: Metadata = {
   title: `Project BD | ${title}`,
 }
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SiteWrapper>
-      <SubHeader title={title} href="/">
-        {children}
-      </SubHeader>
-    </SiteWrapper>
-  )
+  return <SiteWrapper>{children}</SiteWrapper>
 }

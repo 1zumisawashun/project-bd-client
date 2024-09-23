22393f7bc2503b3dcdbd1b0284c5ec1dfe9606ad
1zumisawashun <shun.1zumisawa@gmail.com>
2024-09-23 16:07:05 +0900

    add : loading uiの追加

--- app/(pages)/articles/[id]/(detail)/page.tsx
+++ app/(pages)/articles/[id]/(detail)/page.tsx
@@ -1,10 +1,11 @@
 import { ArticleDetail } from '@/features/articles/ArticleDetail/ArticleDetail'
 import { getArticleById } from '@/functions/db/article'
+import { NotFound } from '@/components/elements/NotFound'
 
 export default async function Page({ params }: { params: { id: string } }) {
   const article = await getArticleById(params.id)
 
-  if (!article) return <div>記事が見つかりませんでした</div>
+  if (!article) return <NotFound />
 
   return <ArticleDetail article={article} />
 }
---
+++ app/(pages)/articles/create/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingSpinner } from '@/components/elements/LoadingSpinner'
+
+export default function Loading() {
+  return <LoadingSpinner />
+}

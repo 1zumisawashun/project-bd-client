c5c0f6626c7c02236c79fd9b6af8552a48b2e8dd
1zumisawashun <shun.1zumisawa@gmail.com>
2024-09-27 20:26:39 +0900

    fix

--- app/features/sign-in/signIn.schema.ts
+++ app/features/sign-in/signIn.schema.ts
@@ -1,7 +1,28 @@
 import { z } from 'zod'
+import isStrongPassword from 'validator/lib/isStrongPassword'
 
+/** @see https://dninomiya.github.io/form-guide/ */
 export const schema = z.object({
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(1, 'This field is required'),
+  email: z
+    .string()
+    .trim()
+    .min(1, {
+      message: 'メールアドレスを入力してください。',
+    })
+    .email({
+      message: 'メールアドレスの形式で入力してください。',
+    })
+    .max(254, {
+      message: '最大254文字までです。',
+    }),
+  password: z
+    .string()
+    .trim()
+    .min(8, {
+      message: '最小8文字以上です。',
+    })
+    .refine(isStrongPassword, {
+      message: '大文字を含む半角英数字と記号を含めてください。',
+    }),
 })
 export type Schema = z.infer<typeof schema>

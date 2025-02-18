b7973a8068fd57b98ea6a67a5b197ce54719f8b7
1zumisawashun <shun.1zumisawa@gmail.com>
2025-02-18 22:44:56 +0900

    fix

--- eslint-plugin-custom-rules/rules/require-satisfies-to-refetch-queries/index.js
+++ eslint-plugin-custom-rules/rules/require-satisfies-to-refetch-queries/index.js
@@ -5,8 +5,23 @@ var utils_1 = require("@typescript-eslint/utils");
 var createRule = utils_1.ESLintUtils.RuleCreator(function () {
     return "https://github.com/1zumisawashun/project-bd-client/blob/main/eslint-plugin-custom-rules/src/require-should-dirty/README.md";
 });
+var isRefetchQueries = function (node) {
+    return (node.key.type === utils_1.AST_NODE_TYPES.Identifier &&
+        node.key.name === 'refetchQueries');
+};
+var isHook = function (node) {
+    return (node.callee.type === utils_1.AST_NODE_TYPES.Identifier &&
+        /^use[A-Z]\w*/.test(node.callee.name));
+};
+var isObjectExpression = function (node) {
+    return node.type === utils_1.AST_NODE_TYPES.ObjectExpression;
+};
+var isProperty = function (node) {
+    return node.type === utils_1.AST_NODE_TYPES.Property;
+};
 /**
  * @see https://typescript-eslint.io/developers/custom-rules/#typed-rules
+ * @see https://www.apollographql.com/docs/react/data/mutations#refetching-queries
  */
 exports.rule = createRule({
     name: 'require-satisfies-to-refetch-queries',
@@ -23,32 +38,35 @@ exports.rule = createRule({
     },
     create: function (context) {
         return {
-            Property: function (node) {
-                if (!isRefetchQueries(node))
-                    return;
-                var parserServices = context.sourceCode.parserServices;
-                if (!(parserServices === null || parserServices === void 0 ? void 0 : parserServices.program) ||
-                    !(parserServices === null || parserServices === void 0 ? void 0 : parserServices.esTreeNodeToTSNodeMap)) {
-                    throw new Error('This rule requires `parserOptions.project`.');
-                }
-                var checker = parserServices.program.getTypeChecker();
-                // TypeScriptのASTノードを取得
-                var tsNode = parserServices.esTreeNodeToTSNodeMap.get(node.value);
-                var type = checker.getTypeAtLocation(tsNode);
-                // `satisfies` が適用されているかをチェック
-                var typeText = checker.typeToString(type);
-                console.log(typeText, '=======');
-                if (!typeText.includes('satisfies')) {
-                    context.report({
-                        node: node,
-                        messageId: 'requireSatisfiesToRefetchQueries',
-                    });
+            CallExpression: function (node) {
+                if (isHook(node)) {
+                    var objectExpression = node.arguments.find(isObjectExpression);
+                    if (!objectExpression)
+                        return;
+                    var property = objectExpression.properties.find(isProperty);
+                    if (!property)
+                        return;
+                    if (isRefetchQueries(property)) {
+                        var parserServices = context.sourceCode.parserServices;
+                        if (!(parserServices === null || parserServices === void 0 ? void 0 : parserServices.program) ||
+                            !(parserServices === null || parserServices === void 0 ? void 0 : parserServices.esTreeNodeToTSNodeMap)) {
+                            throw new Error('This rule requires `parserOptions.project`.');
+                        }
+                        var checker = parserServices.program.getTypeChecker();
+                        // TypeScriptのASTノードを取得
+                        var tsNode = parserServices.esTreeNodeToTSNodeMap.get(property.value);
+                        var type = checker.getTypeAtLocation(tsNode);
+                        // `satisfies` が適用されているかをチェック
+                        var typeText = checker.typeToString(type);
+                        if (!typeText.includes('satisfies')) {
+                            context.report({
+                                node: property,
+                                messageId: 'requireSatisfiesToRefetchQueries',
+                            });
+                        }
+                    }
                 }
             },
         };
     },
 });
-var isRefetchQueries = function (node) {
-    return (node.key.type === utils_1.AST_NODE_TYPES.Identifier &&
-        node.key.name === 'refetchQueries');
-};
--- eslint-plugin-custom-rules/src/require-satisfies-to-refetch-queries/index.ts
+++ eslint-plugin-custom-rules/src/require-satisfies-to-refetch-queries/index.ts
@@ -4,8 +4,31 @@ const createRule = ESLintUtils.RuleCreator(() => {
   return `https://github.com/1zumisawashun/project-bd-client/blob/main/eslint-plugin-custom-rules/src/require-should-dirty/README.md`
 })
 
+const isRefetchQueries = (node: TSESTree.Property) => {
+  return (
+    node.key.type === AST_NODE_TYPES.Identifier &&
+    node.key.name === 'refetchQueries'
+  )
+}
+
+const isHook = (node: TSESTree.CallExpression): boolean => {
+  return (
+    node.callee.type === AST_NODE_TYPES.Identifier &&
+    /^use[A-Z]\w*/.test(node.callee.name)
+  )
+}
+
+const isObjectExpression = (node: TSESTree.Node) => {
+  return node.type === AST_NODE_TYPES.ObjectExpression
+}
+
+const isProperty = (node: TSESTree.Node) => {
+  return node.type === AST_NODE_TYPES.Property
+}
+
 /**
  * @see https://typescript-eslint.io/developers/custom-rules/#typed-rules
+ * @see https://www.apollographql.com/docs/react/data/mutations#refetching-queries
  */
 export const rule = createRule({
   name: 'require-satisfies-to-refetch-queries',
@@ -22,38 +45,41 @@ export const rule = createRule({
   },
   create(context) {
     return {
-      Property(node) {
-        if (!isRefetchQueries(node)) return
+      CallExpression(node) {
+        if (isHook(node)) {
+          const objectExpression = node.arguments.find(isObjectExpression)
+          if (!objectExpression) return
 
-        const parserServices = context.sourceCode.parserServices
-        if (
-          !parserServices?.program ||
-          !parserServices?.esTreeNodeToTSNodeMap
-        ) {
-          throw new Error('This rule requires `parserOptions.project`.')
-        }
-        const checker = parserServices.program.getTypeChecker()
-        // TypeScriptのASTノードを取得
-        const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node.value)
-        const type = checker.getTypeAtLocation(tsNode)
+          const property = objectExpression.properties.find(isProperty)
+          if (!property) return
 
-        // `satisfies` が適用されているかをチェック
-        const typeText = checker.typeToString(type)
-        console.log(typeText, '=======')
-        if (!typeText.includes('satisfies')) {
-          context.report({
-            node: node,
-            messageId: 'requireSatisfiesToRefetchQueries',
-          })
+          if (isRefetchQueries(property)) {
+            const parserServices = context.sourceCode.parserServices
+            if (
+              !parserServices?.program ||
+              !parserServices?.esTreeNodeToTSNodeMap
+            ) {
+              throw new Error('This rule requires `parserOptions.project`.')
+            }
+            const checker = parserServices.program.getTypeChecker()
+            // TypeScriptのASTノードを取得
+            const tsNode = parserServices.esTreeNodeToTSNodeMap.get(
+              property.value,
+            )
+            const type = checker.getTypeAtLocation(tsNode)
+
+            // `satisfies` が適用されているかをチェック
+            const typeText = checker.typeToString(type)
+
+            if (!typeText.includes('satisfies')) {
+              context.report({
+                node: property,
+                messageId: 'requireSatisfiesToRefetchQueries',
+              })
+            }
+          }
         }
       },
     }
   },
 })
-
-const isRefetchQueries = (node: TSESTree.Property) => {
-  return (
-    node.key.type === AST_NODE_TYPES.Identifier &&
-    node.key.name === 'refetchQueries'
-  )
-}

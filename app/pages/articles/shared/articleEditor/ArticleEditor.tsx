3c67bfad59487d1920de032ebdc3f2267a88b054
1zumisawashun <shun.1zumisawa@gmail.com>
2025-12-28 22:35:59 +0900

    fix

--- app/pages/articles/shared/articleEditor/ArticleEditor.tsx
+++ app/pages/articles/shared/articleEditor/ArticleEditor.tsx
@@ -4,9 +4,9 @@ import { EditorContent } from '@/components/elements/Editor'
 import { useEditor } from '@/components/elements/Editor/hooks/useEditor'
 import { VStack } from '@/components/layouts/VStack'
 import { FC } from 'react'
-import { ArticleMenuBubble } from '../articleMenuBubble/ArticleMenuBubble'
-import { ArticleMenubar } from '../articleMenubar/ArticleMenubar'
 import styles from './articleEditor.module.css'
+import { ArticleMenuBubble } from './components/articleMenuBubble/ArticleMenuBubble'
+import { ArticleMenubar } from './components/articleMenubar/ArticleMenubar'
 
 const BLOCK_NAME = 'articleEditor'
 

efc75a672f2efcf37bebbe9636182861594e43e8
1zumisawashun <shun.1zumisawa@gmail.com>
2026-01-19 00:06:53 +0900

    refactor: enhance type definitions for next-auth by adding User and Account interfaces; update Session interface to remove expires property

--- app/@types/next-auth.d.ts
+++ app/@types/next-auth.d.ts
@@ -1,19 +1,36 @@
 import { DefaultSession } from 'next-auth'
+// The `JWT` interface can be found in the `next-auth/jwt` submodule
 import 'next-auth/jwt'
 
 type Role = 'USER' | 'ADMIN'
 
+// https://authjs.dev/getting-started/typescript#module-augmentation
+
 declare module 'next-auth' {
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+  interface User {}
+  /**
+   * The shape of the account object returned in the OAuth providers' `account` callback,
+   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+   */
+  interface Account {}
+
+  /**
+   * Returned by `useSession`, `auth`, contains information about the active session.
+   */
   interface Session {
     user: {
       id: string
       role: Role
-      expires: string
     } & DefaultSession['user']
   }
 }
 
 declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
   interface JWT {
     id: string
     role: Role

96181e94c9f98e762c3aca8048759eee199d0eef
1zumisawashun <shun.1zumisawa@gmail.com>
2026-03-21 13:31:08 +0900

    feat: remove base-button composition and update class names to use 'ui-button'

--- app/components/buttons/IconButton/index.tsx
+++ app/components/buttons/IconButton/index.tsx
@@ -45,7 +45,7 @@ export const IconButton = forwardRef<Ref, Props>(
         // base-ui props
         nativeButton={true}
         // native props
-        className={clsx(styles[`${BLOCK_NAME}`], className)}
+        className={clsx('ui-button', styles[`${BLOCK_NAME}`], className)}
         type={type}
         disabled={disabled}
         ref={ref}
